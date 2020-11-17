cf222c8b0b3be6af5a1a3e5f7229eb53681a3fe1
Vladimir Aridov <aridov@mail.ru>
2020-11-18 00:48:25 +0200

    app setup done

--- SpaceX API Explorer/Home/HomeController.swift
+++ SpaceX API Explorer/Home/HomeController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeController.swift
 //  SpaceX API Explorer
 //
 //  Created by Vladimir Aridov on 18.11.2020.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
-
